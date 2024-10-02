b26148f766c761512f0c3b9c4c2c24c1b3dcaac5
ByronM22 <byronm22@gmail.com>
2024-10-02 12:56:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.subheader("Ingresar los flujos de caja")
 # Ingresar ingresos, costos variables y gastos fijos para cada año
 ingreso_base = st.number_input("Ingresos para el primer año:", value=0.00)
 costo_variable_base = st.number_input("Porcentaje de costo variable para el primer año (%):", value=0.0) / 100
-gasto_fijo_base = st.number_input("Gastos fijos para el primer año:", value=20000.00)
+gasto_fijo_base = st.number_input("Gastos fijos para el primer año:", value=0.00)
 
 for i in range(n_años):
     # Aplicar el crecimiento a ingresos, costos variables y gastos fijos
